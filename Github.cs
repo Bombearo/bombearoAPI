5f316dc7265cfde1c587a7ba52fd8d05a1b6ba3b
Jaden <shekjaden@gmail.com>
2021-12-20 23:12:37 +0000

    Added more fields to github api response that will be used

--- Github.cs
+++ Github.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Caching;
 
 namespace PersonalWebsiteAPI
@@ -12,6 +13,26 @@ namespace PersonalWebsiteAPI
         public string languages_url { get; set; }
 
         public List<Language> Languages { get; set; }
+
+        public bool fork { get; set; }
+        
+        public string html_url { get; set; }
+        
+        public string node_id { get; set; }
+        
+        public string clone_url { get; set; }
+        
+        public string ssh_url { get; set; }
+        
+        public string git_url { get; set; }
+        
+        public int stargazers_count { get; set; }
+        
+        public int watchers_count { get; set; }
+        
+        public DateTime created_at { get; set; }
+        public DateTime pushed_at { get; set; }
+        public DateTime updated_at { get; set; }
     }
 
     public class Language
